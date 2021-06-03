2630604cc683a53dbe0142ed93fab10dfe61e2c0
bsak350 <bsak350@gmail.com>
2021-06-03 13:23:10 -0700

    Added search engine ability

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="Container">
+      <div className="container">
         <h1>Dictionary App</h1>
         <Dictionary />
         <br />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <form>
+      <form onSubmit={search}>
         <input
           type="search"
+          onChange={handleKeywordChange}
           className="form-control w-100"
           placeholder="Type a word"
           autoFocus="on"
