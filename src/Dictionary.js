be9a70808eaace3f20de1d1b3de30d62c4ab6203
bsak350 <bsak350@gmail.com>
2021-06-03 14:38:17 -0700

    Created results state, setResults function, new Results component and functionality, rendering meanings and new Meaning component, added parts of speech, definitions, examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState({});
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -37,6 +39,7 @@ export default function Dictionary() {
           className="form-control btn-primary w-100"
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
