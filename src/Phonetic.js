e439e7da4967bb29936146cb38c8134505c6ec88
bsak350 <bsak350@gmail.com>
2021-06-08 18:35:55 -0700

    Added new font to html, edited font sizes

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,10 +4,12 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
+      <h2>
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <span className="text">{props.phonetic.text}</span>
+      </h2>
     </div>
   );
 }
