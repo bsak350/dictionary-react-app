b077b06980df965c308613766fe7dceaf2864efc
bsak350 <bsak350@gmail.com>
2021-06-08 17:06:40 -0700

    Edited section, input, hint and styling.  Added default keyword.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Dictionary App</h1>
-        <Dictionary />
+        <Dictionary defaultKeyword="dictionary" />
         <br />
         <p>
           Project coded by Brittany Sak
