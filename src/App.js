c85a3b647c1c363ac19c10dc1ca424669b5442fb
bsak350 <bsak350@gmail.com>
2021-06-09 01:38:59 -0700

    Edited styling and default word

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Dictionary App</h1>
-        <Dictionary defaultKeyword="butterfly" />
+        <Dictionary defaultKeyword="island" />
         <br />
         <p>
           Project coded by Brittany Sak
